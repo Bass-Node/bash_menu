5e0df6a895d43a0aded931618c803d25cf764053
Niels <nhuisman@greensea.com>
2019-08-12 17:03:20 -0400

    First commit

---
+++ menu.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# =============================================
+# Niels Huisman 2019
+# Simple menu to quickly access several options
+# =============================================
